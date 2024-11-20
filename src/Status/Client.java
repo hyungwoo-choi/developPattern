d321a16f8f262d8cbcfcfec2176dd0f62953ff15
hyungwoo-choi <tinbed122@gmail.com>
2024-11-20 18:59:08 +0900

    스테이트 패턴 코드

--- src/Status/Client.java
+++ src/Status/Client.java
@@ -3,5 +3,10 @@ package Status;
 public class Client {
     public static void main(String[] args) {
 
+        Light light = new Light();
+        light.off();
+        light.on();
+        light.off();
+
     }
 }
--- src/Status/Light.java
+++ src/Status/Light.java
@@ -8,4 +8,24 @@ public class Light {
     public Light() {
         state = OFF; //형광등 초기 상태는 꺼져있는 상태임
     }
+
+    public void on_button_pushed(){
+        if(state == ON){
+            System.out.println("반응없음");
+        }
+        else{
+            System.out.println("Light On");
+            state = ON;
+        }
+    }
+
+    public void off_button_pushed(){
+        if(state == OFF){
+            System.out.println("반응 없음");
+        }
+        else{
+            System.out.println("Light Off");
+            state = OFF;
+        }
+    }
 }
