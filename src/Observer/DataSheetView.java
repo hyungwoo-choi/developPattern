dcffacae6b54fe15f75a5e69858605a39e2c4869
hyungwoo-choi <tinbed122@gmail.com>
2024-11-25 19:08:29 +0900

    #1 옵저버 패턴 문제점 코드

---
+++ src/Observer/DataSheetView.java
@@ -0,0 +1,24 @@
+package Observer;
+
+import java.util.List;
+
+public class DataSheetView {
+    private ScoreRecord scoreRecord;
+    private int viewCount;
+
+    public DataSheetView(ScoreRecord scoreRecord, int viewCount) {
+        this.scoreRecord = scoreRecord;
+        this.viewCount = viewCount;
+    }
+    public void update(){
+        List<Integer> record = scoreRecord.getScoreRecord();
+        displayScores(record, viewCount);
+    }
+    private void displayScores(List<Integer> record, Integer viewCount){
+        System.out.println("List of" + viewCount + "entries : ");
+        for (int i = 0; i < viewCount && i < record.size(); i++) {
+            System.out.println(record.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
